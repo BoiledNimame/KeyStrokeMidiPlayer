525e2118e923cae77d42da393f941b2ef5682e74
HadukiDesktop <haduki.kyouka@gmail.com>
2024-04-02 13:00:11 +0900

    定数インターフェース

---
+++ src/main/java/com/kmidiplayer/midi/midiCommandType.java
@@ -0,0 +1,7 @@
+package com.kmidiplayer.midi;
+
+public interface midiCommandType {
+    public static final int NOTE_ON = 0;
+    public static final int NOTE_OFF = 1;
+    public static final int SLEEP = 2;
+}
