d38f7617b41c19f6bf9b088c1bf45af6adae6019
HadukiDesktop <haduki.kyouka@gmail.com>
2024-04-02 09:35:51 +0900

    ロード機能に絞る

--- src/main/java/com/kmidiplayer/midi/midiLoader.java
+++ src/main/java/com/kmidiplayer/midi/midiLoader.java
@@ -1,5 +1,12 @@
 package com.kmidiplayer.midi;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MetaMessage;
 import javax.sound.midi.MidiEvent;
@@ -12,106 +19,82 @@ import javax.sound.midi.Track;
 import com.kmidiplayer.App;
 import com.kmidiplayer.gui.Gui;
 import com.kmidiplayer.gui.PrimaryController;
-import com.kmidiplayer.keylogger.KeyboardInput;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
-public class midiLoader extends Thread {
-    // final化出来るように
-    private final KeyboardInput keyboardController;
-
-    public midiLoader(KeyboardInput kinput) {
-        keyboardController = kinput;
-    }
-
-    private static Sequence sequence;
-
-    private static float tickInMilliSeconds;
-    private static List<int[]> keyControlSequences = null;
-
-    public static void loadFile(String midiDirectory) {
+public class midiLoader {
+    public static Sequence getSequencefromDirectory(String midiDirectory) {
+        Gui.logger().info("trying load midi file...");
         try {
-            Gui.logger().info("trying load midi file...");
-            sequence = MidiSystem.getSequence(new File(midiDirectory));
+            final File file = new File(midiDirectory);
+            try {
+                final Sequence sequence = MidiSystem.getSequence(file);
+                if (sequence != null){
+                    Gui.logger().info("midifile load is Done.");
+                }
+                return sequence;
+            } catch (InvalidMidiDataException e) {
+                Gui.logger().info("MIDI data is corrupted or incorrect.");
+                e.printStackTrace();
+                return null;
+            }
         } catch (IOException e) {
             Gui.logger().info("File does not exist or does not have access rights.");
             e.printStackTrace();
-        } catch (InvalidMidiDataException e) {
-            Gui.logger().info("MIDI data is corrupted or incorrect.");
-            e.printStackTrace();
+            return null;
         }
-        if (sequence != null){
-            Gui.logger().info("midifile load is Done.");
+    }
+
+    public static List<MidiEvent> convertSequenceToMidiEvent(Sequence sequence) {
+        if (sequence.getTracks().length != 1) {
+            App.logger().warn("Multiple tracks detected. Operation not guaranteed.");
         }
 
-        // トラックごとにイベントを処理する
-        List<MidiEvent> noteOnEvents = new ArrayList<>();
-        List<MidiEvent> noteOffEvents = new ArrayList<>();
+        // すべてのイベントをtick順にまとめたリスト
+        final List<MidiEvent> allEvents = new ArrayList<>();
+
         for (Track track : sequence.getTracks()) {
             // トラック内のイベントを処理する
             for (int i = 0; i < track.size(); i++) {
                 MidiEvent event = track.get(i);
-                MidiMessage message = event.getMessage();
-                if (message instanceof ShortMessage) {
-                    ShortMessage sm = (ShortMessage) message;
-                    if (sm.getCommand() == ShortMessage.NOTE_ON) {
-                        int velocity = sm.getData2();
-                        if (velocity != 0) {
-                            // NOTE_ONをListに格納
-                            noteOnEvents.add(event);
-                        } else {
-                            // NOTE_OFFをListに格納
-                            noteOffEvents.add(event);
-                        }
-                    } else if (sm.getCommand() == ShortMessage.NOTE_OFF) {
-                        // NOTE_OFFをListに格納
-                        noteOffEvents.add(event);
-                    }
+                if (event.getMessage() instanceof ShortMessage) {
+                    allEvents.add(event);
                 }
+
             }
         }
 
-        float tempoBPM = 120f; // デフォルト値
+        Collections.sort(allEvents, new Comparator<MidiEvent>() {
+            public int compare(MidiEvent event1, MidiEvent event2) {
+                return Math.toIntExact(event1.getTick() - event2.getTick());
+            }
+        });
+
+        double tempoBPM = 120D; // デフォルト値
         
-        // トラックの列挙
-        for (Track track : sequence.getTracks()) {
-            // MIDIイベントの列挙
-            for (int i = 0; i < track.size(); i++) {
-                MidiEvent event = track.get(i);
-                MidiMessage message = event.getMessage();
-                if (message instanceof MetaMessage) {
-                    MetaMessage meta = (MetaMessage) message;
-                    // テンポメッセージの場合
-                    if (meta.getType() == 0x51) {
-                        // テンポを算出
-                        byte[] data = meta.getData();
-                        int tempo = (data[0] & 0xFF) << 16 | (data[1] & 0xFF) << 8 | (data[2] & 0xFF);
-                        tempoBPM = 60000000f / tempo;
+        GET_TEMPO : {
+            for (Track track : sequence.getTracks()) {
+                for (int i = 0; i < track.size(); i++) {
+                    MidiEvent event = track.get(i);
+                    MidiMessage message = event.getMessage();
+                    if (message instanceof MetaMessage) {
+                        MetaMessage meta = (MetaMessage) message;
+    
+                        final int TEMPO_MESSAGE = 0x51;
+                        if (meta.getType() == TEMPO_MESSAGE) {
+                            // テンポを算出
+                            byte[] data = meta.getData();
+                            int tempo = (data[0] & 0xFF) << 16 | (data[1] & 0xFF) << 8 | (data[2] & 0xFF);
+                            tempoBPM = 60000000D / tempo;
+                            break GET_TEMPO;
+                        }
                     }
                 }
             }
         }
 
         // 1tickの秒数計算
-        tickInMilliSeconds =  60f / (sequence.getResolution() * tempoBPM);
+        final double tickInMilliSeconds =  60D / (sequence.getResolution() * tempoBPM);
 
         Gui.logger().info("1 tick = " + tickInMilliSeconds + " millisecond");
-
-        // NOTE_ONとNOTE_OFF eventを時間順にソートし
-        // すべてのイベントをtick順にまとめたリストとして再構築する
-        List<MidiEvent> allEvents = new ArrayList<>();
-        allEvents.addAll(noteOnEvents);
-        allEvents.addAll(noteOffEvents);
-        Collections.sort(allEvents, new Comparator<MidiEvent>() {
-            public int compare(MidiEvent event1, MidiEvent event2) {
-                return (int) (event1.getTick() - event2.getTick());
-            }
-        });
 
         // Listの内容を確認する(at debug)
         if(App.getKeyInput().isDebug()){
@@ -119,92 +102,69 @@ public class midiLoader extends Thread {
                 Gui.logger().debug("NOTE_"+ ((ShortMessage) event.getMessage()).getData2() + "_" + ((ShortMessage) event.getMessage()).getData1() + " at tick :" + event.getTick());
             }
         }
+        return allEvents;
+    }
 
-        // 時間順にNOTE_ON,OFF,Thread.sleep(ms)をまとめたListを作成
-        int prevTick =0;
-        int diff =0;
-        List<int[]> keyControlEventStuck = new ArrayList<>();
+    static final int NOTE_ON = 0;
+    static final int NOTE_OFF = 1;
+    static final int SLEEP = 2;
+    public static List<long[]> convertRawKeys(List<MidiEvent> rawEvents) {
         // eventStuck int[ type, note, tick, diff, sequencesIndexNumber ]
-        // type: on=1 off=1
-        for (MidiEvent event : allEvents) {
+        final List<long[]> keyControlEventStuck = new ArrayList<>();
+        CONVERT : for (int index = 0; index < rawEvents.size(); index++) {
+            final MidiEvent event = rawEvents.get(index);
+            //-------------------------type-------------------------//
             int type;
-            int note = ((ShortMessage) event.getMessage()).getData1();
-            int tick = (int) event.getTick();
-            if (prevTick == (int) event.getTick()){
-                diff = 0;
-            } else {
-                diff = (int) event.getTick() - prevTick;
-            }
-            prevTick = (int) event.getTick();
-
             if (event.getMessage() instanceof ShortMessage) {
-                ShortMessage sm = (ShortMessage) event.getMessage();
-                if (sm.getCommand() == ShortMessage.NOTE_ON) {
-                    type = 0;
-                } else if (sm.getCommand() == ShortMessage.NOTE_OFF) {
-                    type = 1;
-                } else {
-                    // ノート以外のイベントはスキップ
-                    continue;
+                switch (((ShortMessage) event.getMessage()).getCommand()) {
+                    case ShortMessage.NOTE_ON:
+                        type = NOTE_ON;
+                        break;
+
+                    case ShortMessage.NOTE_OFF:
+                        type = NOTE_OFF;
+                        break;
+
+                    default:
+                        continue CONVERT;
                 }
             } else {
-                // ノート以外のイベントはスキップ
-                continue;
+                continue CONVERT;
             }
-            keyControlEventStuck.add(new int[]{type, note, tick, diff});
+            //-------------------------note-------------------------//
+            int note = ((ShortMessage) event.getMessage()).getData1();
+            //-------------------------tick-------------------------//
+            long tick = event.getTick();
+            //-------------------------diff-------------------------//
+            long diff;
+            if (index != 0) {
+                final MidiEvent prevEvent = rawEvents.get(index-1);
+                if (prevEvent.getTick() == event.getTick()){
+                    diff = 0;
+                } else {
+                    diff = event.getTick() - prevEvent.getTick();
+                }
+            } else {
+                diff = event.getTick();
+            }
+            //------------------------return------------------------//
+            keyControlEventStuck.add(new long[]{type, note, tick, diff});
         }
 
         // 遅延を追加し新しいリストに格納する
-        // 新しいリストではint[type, note/sleep]となる
-        // type=0(on)/1(off)/2(sleep),sleep=ticks
-
-        keyControlSequences = new ArrayList<>();
-        for (int[] event : keyControlEventStuck){
+        // 新しいリストではlong[type, note/sleep]となる
+        final List<long[]> keyControlSequences = new ArrayList<>();
+        for (long[] event : keyControlEventStuck){
+            // ひとつ前のコマンドからの経過時間
             if (event[3] != 0){
-                keyControlSequences.add(new int[]{2,event[3]});
-                keyControlSequences.add(new int[]{event[0],event[1]});
+                keyControlSequences.add(new long[]{SLEEP, event[3]});
+                keyControlSequences.add(new long[]{event[0], event[1]});
             } else {
-                keyControlSequences.add(new int[]{event[0],event[1]});
+                keyControlSequences.add(new long[]{event[0], event[1]});
             }
         }
+        // これ自体はFx Thread以外から呼ばれることを意図していない
         PrimaryController.IsFileLoadSucsessSetter(true);
-    }
-
-    @Override
-    public void run() {
-        for (int[] event : keyControlSequences){
-            switch (event [0]) {
-                case 0: // NOTE_ON
-                    keyboardController.KeyControl(event[1],true);
-                    break;
-                case 1: // NOTE_OFF
-                    keyboardController.KeyControl(event[1],false);
-                    break;
-                case 2: // SLEEP
-                    try {
-                        if(keyboardController.isDebug()){
-                            Gui.logger().debug("Try to Sleep "+(long)(event[1]*(tickInMilliSeconds*1000))+"MiliSeconds");
-                        }
-                        Thread.sleep((long)(event[1]*(tickInMilliSeconds*1000)));
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                default:
-                    Gui.logger().info("Invailed type Number!");
-                    break;
-            }
-        }
-        Gui.logger().info("Output is Ended, result:");
-        Gui.logger().info("MaxOutOfRange:" + keyboardController.occurrencesOfOutOfRangeMax);
-        Gui.logger().info("Maximum Max difference :" + keyboardController.valeOfOutOfRangeMax);
-        Gui.logger().info("MinOutOfRange:" + keyboardController.occurrencesOfOutOfRangeMin);
-        Gui.logger().info("Maximum Min difference :" + keyboardController.valeOfOutOfRangeMin);
-        // 次の再生に備えリセットする
-        keyboardController.occurrencesOfOutOfRangeMax=0;
-        keyboardController.occurrencesOfOutOfRangeMin=0;
-        keyboardController.valeOfOutOfRangeMax = 0;
-        keyboardController.valeOfOutOfRangeMin = 0;
-
+        return keyControlSequences;
     }
 }
