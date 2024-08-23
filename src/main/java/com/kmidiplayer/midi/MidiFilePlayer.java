a6b5edc39f3cfbf148a464e3903b133824208343
HadukiDesktop <haduki.kyouka@gmail.com>
2024-08-24 06:45:38 +0900

    コミット抜け

---
+++ src/main/java/com/kmidiplayer/midi/MidiFilePlayer.java
@@ -0,0 +1,63 @@
+package com.kmidiplayer.midi;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+
+import com.kmidiplayer.application.Main;
+import com.kmidiplayer.midi.data.PlayerTask;
+import com.kmidiplayer.midi.util.MidiFileChecker;
+import com.kmidiplayer.midi.util.NoteConverter;
+
+public class MidiFilePlayer {
+
+    private final Sequence sequence;
+    private final ScheduledExecutorService executor;
+    private Future<?> future;
+
+    public MidiFilePlayer(File file) {
+        if (MidiFileChecker.isValid(file)) {
+            try {
+                sequence = MidiSystem.getSequence(file);
+                executor = Executors.newSingleThreadScheduledExecutor();
+            } catch (InvalidMidiDataException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            sequence = null;
+            executor = null;
+        }
+    }
+
+    private boolean hasValidData() {
+        return Objects.nonNull(sequence);
+    }
+
+    public void play(int[] tracks, int initialDelay) {
+        if (!hasValidData()) { return; }
+        future = executor.scheduleAtFixedRate(
+                    new PlayerTask(
+                        Main.getKeyInput(),
+                        this,
+                        NoteConverter.convert(tracks, sequence),
+                        initialDelay),
+                    initialDelay,
+                    (sequence.getMicrosecondLength() / sequence.getTickLength()) / 1000,
+                    TimeUnit.MILLISECONDS);
+    }
+
+    public void cancel() {
+        if (future != null) {
+            future.cancel(true);
+        }
+    }
+
+}
