b388a18bc19dd1b2d0da9963a6b9d5f4f1bb9c6f
HadukiDesktop <haduki.kyouka@gmail.com>
2024-10-10 16:23:19 +0900

    ハードコート修正

--- src/main/java/com/kmidiplayer/keylogger/KeycordMap.java
+++ src/main/java/com/kmidiplayer/keylogger/KeycordMap.java
@@ -1,60 +1,27 @@
 package com.kmidiplayer.keylogger;
 
+import java.util.AbstractMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import com.kmidiplayer.config.YamlLoader;
 
 import java.util.HashMap;
 
 public class KeycordMap {
-    private static int returnKey = 65;
-    private static Map<String, Integer> keyCodeMap = new HashMap<String, Integer>(){ {
-            // Code (仮想キーコード)から
-            // reference : https://kts.sakaiweb.com/virtualkeycodes.html
-        put ("0",48);
-        put ("1",49);
-        put ("2",50);
-        put ("3",51);
-        put ("4",52);
-        put ("5",53);
-        put ("6",54);
-        put ("7",55);
-        put ("8",56);
-        put ("9",57);
+    private final static String resourceLocation = "/vkcode.yaml";
 
-        put ("A",65);
-        put ("B",66);
-        put ("C",67);
-        put ("D",68);
-        put ("E",69);
-        put ("F",70);
-        put ("G",71);
-        put ("H",72);
-        put ("I",73);
-        put ("J",74);
-        put ("K",75);
-        put ("L",76);
-        put ("M",77);
-        put ("N",78);
-        put ("O",79);
-        put ("P",80);
-        put ("Q",81);
-        put ("R",82);
-        put ("S",83);
-        put ("T",84);
-        put ("U",85);
-        put ("V",86);
-        put ("W",87);
-        put ("X",88);
-        put ("Y",89);
-        put ("Z",90);
-    } };
-    
+    private final static Map<String, Integer> keyCodeMap =
+        YamlLoader.loadAsMap(resourceLocation).entrySet().stream()
+                  .map(s -> new AbstractMap.SimpleEntry<String, Integer>(s.getKey().toString(), Integer.valueOf(s.getValue().toString())))
+                  .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k1, k2) -> k1, HashMap::new));
+
     public static int GetVKcode(String key){
-        // key(str) => vkcode(int)に変換
-        if(keyCodeMap.get(key) == null){
-            returnKey = 65;
+        if(Objects.nonNull(keyCodeMap.get(key)) || keyCodeMap.containsKey(key)){
+            return (int) keyCodeMap.get(key);
         } else {
-            returnKey = (int) keyCodeMap.get(key);
+            return 65;
         }
-        return returnKey;
     }
 }
