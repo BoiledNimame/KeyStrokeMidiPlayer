eea8794c8965132b0df30ea5c4dd4681e95504ef
HadukiLaptop <haduki.kyouka@gmail.com>
2024-10-16 12:14:19 +0900

    途中まで作った

--- src/main/java/com/kmidiplayer/gui/MUIView.java
+++ src/main/java/com/kmidiplayer/gui/MUIView.java
@@ -6,9 +6,12 @@ import org.apache.logging.log4j.Logger;
 import com.kmidiplayer.application.Main;
 import com.kmidiplayer.util.Resource;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 // TODO MaterialFxによるViewを作る
@@ -38,14 +41,6 @@ public class MUIView {
             /*
              * <AnchorPane maxHeight="-Infinity" maxWidth="-Infinity" minHeight="-Infinity" minWidth="-Infinity" prefHeight="384.0" prefWidth="600.0" xmlns="http://javafx.com/javafx/18" xmlns:fx="http://javafx.com/fxml/1">
              *    <children>
-             *       <AnchorPane layoutX="14.0" layoutY="42.0" prefHeight="220.0" prefWidth="350.0" style="-fx-background-color: #D3D3D3;" AnchorPane.leftAnchor="15.0" AnchorPane.topAnchor="40.0">
-             *          <children>
-             *             <Text layoutX="127.0" layoutY="123.0" strokeType="OUTSIDE" strokeWidth="0.0" text="Drag &amp; Drop here" />
-             *             <Text layoutX="169.0" layoutY="106.0" strokeType="OUTSIDE" strokeWidth="0.0" text="↑" />
-             *          </children>
-             *       </AnchorPane>
-             *       <TextField layoutX="14.0" layoutY="17.0" prefHeight="25.0" prefWidth="285.0" promptText="path" AnchorPane.leftAnchor="15.0" AnchorPane.topAnchor="15.0" />
-             *       <Button layoutX="300.0" layoutY="17.0" mnemonicParsing="false" prefHeight="25.0" prefWidth="65.0" text="reset" AnchorPane.leftAnchor="300.0" AnchorPane.topAnchor="15.0" />
              *       <Button layoutX="189.0" layoutY="344.0" mnemonicParsing="false" prefHeight="20.0" prefWidth="175.0" text="Stop" AnchorPane.bottomAnchor="15.0" AnchorPane.leftAnchor="190.0" />
              *       <Button layoutX="13.0" layoutY="269.0" mnemonicParsing="false" prefHeight="20.0" prefWidth="175.0" text="Play" AnchorPane.bottomAnchor="15.0" AnchorPane.leftAnchor="15.0" />
              *       <TextField layoutX="189.0" layoutY="310.0" prefHeight="25.0" prefWidth="175.0" promptText="delay (seconds)" AnchorPane.bottomAnchor="50.0" AnchorPane.leftAnchor="190.0" />
@@ -61,6 +56,32 @@ public class MUIView {
              * </AnchorPane>
              * 
             */
+            final Text dropText1 = new Text("↑");
+             dropText1.setLayoutX(169.0D);
+             dropText1.setLayoutY(106.0D);
+            final Text dropText2 = new Text("Drag & Drop here");
+             dropText2.setLayoutX(127.0D);
+             dropText2.setLayoutY(123.0D);
+            final AnchorPane fileDropArea = new AnchorPane(dropText1, dropText2);
+             fileDropArea.setPrefHeight(220.0D);
+             fileDropArea.setPrefWidth(350.0D);
+             fileDropArea.setLayoutX(14.0D);
+             fileDropArea.setLayoutY(42.0D);
+             fileDropArea.setStyle("-fx-background-color: #D3D3D3;");
+            final TextField midPathField = new TextField();
+             midPathField.setLayoutX(14.0D);
+             midPathField.setLayoutY(17.0D);
+             midPathField.setPromptText("path");
+             midPathField.setPrefHeight(25.0D);
+             midPathField.setPrefWidth(285.0D);
+            final Button pathReset = new Button();
+             pathReset.setLayoutX(300.0D);
+             pathReset.setLayoutY(17.0D);
+             pathReset.setPrefHeight(25.0D);
+             pathReset.setPrefWidth(65.0D);
+             pathReset.setText("reset");
+
+        BASE.getChildren().addAll(fileDropArea, midPathField, pathReset);
     }
 
     public Pane getBasePane() {
