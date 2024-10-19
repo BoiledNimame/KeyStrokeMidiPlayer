1001b42190ee0128eb52c6e57002cbb4583eef5f
HadukiDesktop <haduki.kyouka@gmail.com>
2024-10-19 23:21:21 +0900

    なんかデフォルトのcssがあるっぽいぞ

--- src/main/java/com/kmidiplayer/gui/MUIView.java
+++ src/main/java/com/kmidiplayer/gui/MUIView.java
@@ -1,5 +1,7 @@
 package com.kmidiplayer.gui;
 
+import java.util.Arrays;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -7,11 +9,12 @@ import com.kmidiplayer.application.Main;
 import com.kmidiplayer.config.ConfigHolder;
 import com.kmidiplayer.util.Resource;
 
-import javafx.scene.control.Button;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.scene.Parent;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -44,74 +47,96 @@ public class MUIView {
         BASE = new AnchorPane();
         BASE.setStyle("-fx-background-color: white");
             final Text dropText1 = new Text("↑");
+             dropText1.setId("Text_Drop");
              dropText1.setLayoutX(169.0D);
              dropText1.setLayoutY(106.0D);
             final Text dropText2 = new Text("Drag & Drop here");
+             dropText2.setId("Text_Drop");
              dropText2.setLayoutX(127.0D);
              dropText2.setLayoutY(123.0D);
             final AnchorPane fileDropArea = new AnchorPane(dropText1, dropText2);
+             fileDropArea.setId("AnchorPane_DropArea");
              fileDropArea.setPrefHeight(220.0D);
              fileDropArea.setPrefWidth(350.0D);
              fileDropArea.setLayoutX(14.0D);
              fileDropArea.setLayoutY(42.0D);
              fileDropArea.setStyle("-fx-background-color: #D3D3D3;");
-            final TextField midPathField = new TextField();
+            final MFXTextField midPathField = new MFXTextField();
+             midPathField.setId("TextField_MPath");
              midPathField.setLayoutX(14.0D);
              midPathField.setLayoutY(17.0D);
-             midPathField.setPromptText("path");
              midPathField.setPrefHeight(25.0D);
              midPathField.setPrefWidth(285.0D);
-            final Button pathReset = new Button();
+             midPathField.setPromptText("path");
+             midPathField.setFloatingText("path");
+            final MFXButton pathReset = new MFXButton();
+            pathReset.setId("Button_Reset");
              pathReset.setLayoutX(300.0D);
              pathReset.setLayoutY(17.0D);
              pathReset.setPrefHeight(25.0D);
              pathReset.setPrefWidth(65.0D);
              pathReset.setText("reset");
-            final Button playButton = new Button();
+            final MFXButton playButton = new MFXButton();
+            playButton.setId("Button_Play");
              playButton.setLayoutX(13.0D);
              playButton.setLayoutY(344.0D);
              playButton.setPrefHeight(20.0D);
              playButton.setPrefWidth(175.0D);
              playButton.setText("Play");
-            final Button stopButton = new Button();
+            final MFXButton stopButton = new MFXButton();
+            stopButton.setId("Button_Stop");
              stopButton.setLayoutX(189.0D);
              stopButton.setLayoutY(344.0D);
              stopButton.setPrefHeight(20.0D);
              stopButton.setPrefWidth(175.0D);
              stopButton.setText("Stop");
-            final TextField inputDelay = new TextField();
+            final MFXTextField inputDelay = new MFXTextField();
+            inputDelay.setId("TextField_Input");
              inputDelay.setLayoutX(189.0D);
              inputDelay.setLayoutY(310.0D);
              inputDelay.setPrefHeight(25.0D);
              inputDelay.setPrefWidth(175.0D);
-             inputDelay.setPromptText("delay (sec)");
-            final TextField windowName = new TextField(ConfigHolder.configs.getWindowName());
+             inputDelay.setFloatingText("delay (sec)");
+            final MFXTextField windowName = new MFXTextField(ConfigHolder.configs.getWindowName());
+            inputDelay.setId("TextField_WName");
              windowName.setLayoutX(189.0D);
              windowName.setLayoutY(274.0D);
              windowName.setPrefHeight(25.0D);
              windowName.setPrefWidth(175.0D);
              windowName.setPromptText("window name");
+             windowName.setFloatingText("window name");
             final CheckBox useHighPrecision = new CheckBox();
+             useHighPrecision.setId("CheckBox_useHP");
              useHighPrecision.setLayoutX(15.0D);
              useHighPrecision.setLayoutY(314.0D);
              useHighPrecision.setText("use high-precision mode");
             final VBox trackSelectoHolderPane = new VBox();
+             trackSelectoHolderPane.setId("VBox_TrackHolder");
              trackSelectoHolderPane.setPrefHeight(400.0D);
              trackSelectoHolderPane.setPrefWidth(185.0D);
             final ScrollPane trackSelectorHolderWrapperPane = new ScrollPane(trackSelectoHolderPane);
+             trackSelectorHolderWrapperPane.setId("ScrollPane_HolderWrapper");
              trackSelectorHolderWrapperPane.setPrefHeight(329.0D);
              trackSelectorHolderWrapperPane.setPrefWidth(200.0D);
              AnchorPane.setRightAnchor(trackSelectorHolderWrapperPane, 15.0D);
              AnchorPane.setBottomAnchor(trackSelectorHolderWrapperPane, 15.0D);
             final Label trackSelectorLabel = new Label();
+             trackSelectorLabel.setId("Text_TSelector");
              trackSelectorLabel.setText("tracks");
              AnchorPane.setRightAnchor(trackSelectorLabel, 180.0D);
              AnchorPane.setTopAnchor(trackSelectorLabel, 20.0D);
         BASE.getChildren().addAll(fileDropArea, midPathField, pathReset, playButton, stopButton, inputDelay, windowName, useHighPrecision, trackSelectorLabel, trackSelectorHolderWrapperPane);
+
+        addStyleSheetAll(MUIView.class.getResource("View.css").toExternalForm(), BASE.getChildren().toArray(Parent[]::new));
     }
 
     public Pane getBasePane() {
         return BASE;
+    }
+
+    private static void addStyleSheetAll(String style, Parent[] node) {
+        Arrays.stream(node)
+              .forEach(n -> n.getStylesheets().add(style));
     }
 
 }
