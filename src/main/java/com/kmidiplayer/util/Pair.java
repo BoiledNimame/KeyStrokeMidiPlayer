eebcb060e41bec0cd5be25aea62fa7e73c51cffb
HadukiLaptop <haduki.kyouka@gmail.com>
2024-11-05 10:41:19 +0900

    半分ネタ

--- src/main/java/com/kmidiplayer/util/Pair.java
+++ src/main/java/com/kmidiplayer/util/Pair.java
@@ -1,5 +1,7 @@
 package com.kmidiplayer.util;
 
+import java.util.function.Function;
+
 public class Pair<K, V> {
     private final K key;
     private final V val;
@@ -9,4 +11,10 @@ public class Pair<K, V> {
     }
     public K getKey() { return key; }
     public V getValue() { return val; }
+    public static <T, K, V> Function<T, Pair<K, V>> of(
+        Function<? super T, ? extends K> keyMapper,
+        Function<? super T, ? extends V> valueMapper
+        ) {
+        return (km) -> { return new Pair<>(keyMapper.apply(km), valueMapper.apply(km)); };
+    }
 }
