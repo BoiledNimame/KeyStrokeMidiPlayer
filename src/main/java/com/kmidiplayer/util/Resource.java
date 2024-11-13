b845b0469776aa705d23dbe4f1953ede867a1980
HadukiDesktop <haduki.kyouka@gmail.com>
2024-11-14 02:28:19 +0900

    File f = new File(resourceUrl.toURI()); //これはだめらしい

--- src/main/java/com/kmidiplayer/util/Resource.java
+++ src/main/java/com/kmidiplayer/util/Resource.java
@@ -1,9 +1,6 @@
 package com.kmidiplayer.util;
 
 import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 public final class Resource {
 
@@ -15,14 +12,6 @@ public final class Resource {
             return file.getName().split(EXTENSION_STRING)[file.getName().split(EXTENSION_STRING).length - 1];
         } else {
             return EMPTY_STRING;
-        }
-    }
-
-    public static URI getURI(URL url) {
-        try {
-            return url.toURI();
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
         }
     }
 
