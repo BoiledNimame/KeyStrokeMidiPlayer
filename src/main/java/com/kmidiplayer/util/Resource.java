94669873a32a6d0ffcf15b42c621878dbb9907e1
HadukiDesktop <haduki.kyouka@gmail.com>
2024-11-14 02:12:04 +0900

    差し戻した(何？)

--- src/main/java/com/kmidiplayer/util/Resource.java
+++ src/main/java/com/kmidiplayer/util/Resource.java
@@ -1,6 +1,9 @@
 package com.kmidiplayer.util;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public final class Resource {
 
@@ -12,6 +15,14 @@ public final class Resource {
             return file.getName().split(EXTENSION_STRING)[file.getName().split(EXTENSION_STRING).length - 1];
         } else {
             return EMPTY_STRING;
+        }
+    }
+
+    public static URI getURI(URL url) {
+        try {
+            return url.toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
     }
 
