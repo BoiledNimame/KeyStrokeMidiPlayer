890b793abc870d717b5d6e836c2d5381d6bc5bf4
HadukiDesktop <haduki.kyouka@gmail.com>
2024-11-17 17:48:40 +0900

    分ける意味がないので統合

--- src/main/java/com/kmidiplayer/midi/util/MidiFileChecker.java
+++ src/main/java/com/kmidiplayer/midi/util/MidiFileChecker.java
@@ -6,14 +6,12 @@ import java.io.IOException;
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiSystem;
 
-import com.kmidiplayer.util.Resource;
-
 import io.github.palexdev.materialfx.utils.StringUtils;
 
 public class MidiFileChecker {
 
     public static boolean isValid(File file) {
-        final String fileExtension = Resource.getFileExtension(file);
+        final String fileExtension = getFileExtension(file);
         if (StringUtils.EMPTY.equals(fileExtension)) {
             return false;
         } else {
@@ -26,6 +24,17 @@ public class MidiFileChecker {
             } else {
                 return false;
             }
+        }
+    }
+
+    private static final String EMPTY_STRING = "";
+    private static final String EXTENSION_STRING = "\\.";
+
+    public static String getFileExtension(File file) {
+        if (file.isFile()) {
+            return file.getName().split(EXTENSION_STRING)[file.getName().split(EXTENSION_STRING).length - 1];
+        } else {
+            return EMPTY_STRING;
         }
     }
 
